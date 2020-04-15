3435ff9d3675b3e13e2c043674a9828bb793c599
Patrick A <akolo@hotmail.com>
2020-04-15 20:04:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,10 +8,13 @@ def toadstool
   puts $status
 end
 
+<<<<<<< HEAD
 def link 
   puts "It's Dangerous To Go Alone! Take This."   
 end 
 
+=======
+>>>>>>> 90a585ebc1598ad7192dceaacf1a3e8ac7d1c4cc
 def any_phrase (phrase)
   phrase = "Do A Barrel Roll!"
   puts phrase
